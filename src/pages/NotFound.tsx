8100d294f94a71c4760364db7ded89281c6b9bc6
Kei-Taros <orz.www127@gmail.com>
2025-01-08 00:06:18 +0900

    storybook導入

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div>
       <h1>404 - Not Found</h1>
+      <nav>
+        <Link to="/">Back to Home</Link>
+      </nav>
     </div>
   );
 };
